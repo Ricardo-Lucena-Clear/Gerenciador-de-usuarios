0a129422907a81263ac76aaa16ce194c594e278b
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-07 11:05:36 -0300

    Class and MVC

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var fields = document.querySelectorAll ("#form-user-create[name]");
+var fields = document.querySelectorAll ("#form-user-create [name]");
 var user = {};
 
+function addLine(dataUser) {
+    console.log(dataUser);
+    
+    document.getElementById("table-users").innerHTML= `
+     <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+         <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+         <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `;
+
+}
+
 document.getElementById("form-user-create").addEventListener("submit", function(event){
+
     event.preventDefault();
 
 
@@ -10,7 +30,7 @@ fields.forEach (function(fields, index){
     if (fields.name =="gender"){
         
         if (fields.checked){
-            user.gender
+            user[fields.name]= fields.value;
         }
         
 
@@ -18,7 +38,17 @@ fields.forEach (function(fields, index){
         user[fields.name] = fields.value;
     }
     
-})
-console.log(user);
-})
+});
+
+var objectUser = new User (user.name, 
+    user.gender, 
+    user.birth, 
+    user.country, 
+    user.email, 
+    user.passaword, 
+    user.photo, 
+    user.admin
+    );
+addLine(objectUser);
+});
 
