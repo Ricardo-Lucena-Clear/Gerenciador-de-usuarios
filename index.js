be2cecf270d77ece0dd8f71f66bf8e7e0e8a9f75
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-07 09:14:32 -0300

    ForEach

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var nome = document.querySelector ("#exampleInputName");
-var gender = document.querySelectorAll ("#form-user-create [name=gender] : checked");
+var gender = document.querySelectorAll ("#form-user-create [name=gender]");
 var birth = document.querySelector ("#exampleInputBirth");
 var country = document.querySelector ("#exampleInputCountry");
 var email = document.querySelector ("#exampleInputEmail");
 var password = document.querySelector ("#exampleInputPassword");
 var photo = document.querySelector ("#exampleInputFile");
 var admin = document.querySelector ("#exampleInputAdmin");
+
+var fields = document.querySelectorAll ("#form-user-create [name]");
+fields.forEach (function(fields, index){
+    console.log(fields,index);
+})
