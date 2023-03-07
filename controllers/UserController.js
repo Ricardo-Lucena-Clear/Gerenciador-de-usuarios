55dd78c41101095de1994340d1d1108403598719
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-07 12:44:18 -0300

    Promisse in JavaScript

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,36 +13,47 @@ class UserController {
 
             let values = this.getValues();
 
-            
-            this.getPhoto((content)=>{
-
-              values.photo= content;
+            this.getPhoto(). then(
+                (content)=>{
+            values.photo= content;
             this.addLine(values);
 
-            });
+            }, 
+           (e)=>{
+                console.error(e);
 
-            
-        
+            }
+            );       
         });
     }
     
-    getPhoto(callback){
-        let fileReader = new FileReader ();
+    getPhoto(){
 
-       let elements = [...this.formE1.elements].filter(item=>{
-            if (item.name === 'photo'){
-                return item;
-            }
+        return new Promise ((resolve, reject)=>{
+            let fileReader = new FileReader ();
+
+            let elements = [...this.formE1.elements].filter(item=>{
+                 if (item.name === 'photo'){
+                     return item;
+                 }
+             });
+     
+             let file = elements[0].files[0];
+     
+             fileReader.onload = ()=>{
+                 
+             resolve(fileReader.result);
+     
+             }
+
+             fileReader.onerror = (e)=>{
+                reject(e);
+             }
+
+             fileReader.readAsDataURL(file);
+
         });
-
-        let file = elements[0].files[0];
-
-        fileReader.onload = ()=>{
-            
-        callback(fileReader.result);
-
-        }
-        fileReader.readAsDataURL(file);
+  
     }
 
 
