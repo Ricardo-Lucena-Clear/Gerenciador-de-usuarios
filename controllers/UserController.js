401cbee192338074a3ef3ac8c8f8607ba8cb96db
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-08 12:16:12 -0300

    Editing values ​​with Radio and Checkbox

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -166,12 +166,31 @@ class UserController {
             let form = document.querySelector("#form-user-update");
 
             for (let name in json) {
-                let field = form.querySelector("[name=" + name.replace("_","") + "]");
 
-                if (field){
-                    if (field.type == 'file') continue;
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+                if (field) {
+
+                    switch (field.type) {
+                        case 'file':
+                            continue;
+                            break;
+                        case 'radio':
+                            field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+                            field.checked = true;
+                        break;
+                        case 'checkbox':
+                            field.checked = json[name];
+                        break;
+
+                        default:
+                            field.value = json[name];
+
+                    }
+
                     field.value = json[name];
-                }        
+                }
+
             }
             this.showPanelUpdate();
 
